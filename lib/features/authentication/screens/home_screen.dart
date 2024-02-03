6e3a28cc3abda344b39ec6493ec17d3e9f93a1da
NirashaNirmani <nishinirmani@gmail.com>
2024-02-04 03:09:43 +0530

    Add Location_Horizontal containers

--- lib/features/authentication/screens/home_screen.dart
+++ lib/features/authentication/screens/home_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 237, 236, 241),
       body: Stack(
         children: [
           Column(
@@ -26,7 +27,7 @@ class HomeScreen extends StatelessWidget {
                   ),
                   child: Container(
                     decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 255, 255, 255),
+                      color: Color.fromARGB(255, 237, 236, 241),
                       borderRadius:
                           BorderRadius.only(topLeft: Radius.circular(70)),
                     ),
@@ -41,35 +42,115 @@ class HomeScreen extends StatelessWidget {
             right: 0,
             child: Padding(
               padding: const EdgeInsets.all(50.0),
-              child: Container(
-                height: 60,
-                width: 50, // Adjust the height as needed
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 1,
-                      blurRadius: 5,
-                      offset: const Offset(0, 3),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      contentPadding:
-                          const EdgeInsets.symmetric(horizontal: 20),
-                      hintText: 'Search...',
-                      border: InputBorder.none,
-                      icon: Icon(Icons.search),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Row(
+                        children: const [
+                          Icon(Icons.search),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Search...',
+                                hintStyle: TextStyle(fontSize: 15),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
+                  const SizedBox(height: 60),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 30,
+                        height: 30,
+                        child: Icon(Icons.location_on,
+                            size: 35), // Adjust size here
+                      ),
+                      const SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Home',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            '242, Market place, Finland',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w300),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(width: 60),
+                      Column(
+                        children: const [
+                          Icon(Icons.menu_open_outlined),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildIconButton(Icons.fastfood_outlined, 'All'),
+                      _buildIconButton(Icons.local_pizza_outlined, 'Pizza'),
+                      _buildIconButton(Icons.local_drink_sharp, 'Beverages'),
+                      _buildIconButton(
+                          Icons.radio_button_checked_outlined, 'Asian'),
+                    ],
+                  ),
+                ],
               ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  GestureDetector _buildIconButton(IconData icon, String label) {
+    return GestureDetector(
+      onTap: () {
+        // Handle button tap
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Icon(icon, color: const Color.fromARGB(255, 138, 136, 136)),
+          ),
+          const SizedBox(height: 8), // Adjust spacing between icon and label
+          Text(
+            label,
+            style: const TextStyle(fontSize: 12),
           ),
         ],
       ),
